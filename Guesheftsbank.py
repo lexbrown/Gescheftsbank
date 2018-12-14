c180cb1fc309ad9474cfcec2e2e0825c925a31ab
LexBrown <44871845+lexbrown@users.noreply.github.com>
2018-12-14 12:36:14 +0300

    Add files via upload

---
+++ Guesheftsbank.py
@@ -0,0 +1,11 @@
+#MVP Stage1 - 3-6-3
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+
+capital = 100000
+liabilities = 0
+assets = capital + liabilities
+while wotw != "exit":
+	for i in range(5):
