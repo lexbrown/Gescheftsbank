7669da04ba847b48c5410bf9b44e2f2d7f51c02e
LexBrown <44871845+lexbrown@users.noreply.github.com>
2018-12-23 20:26:52 +0300

    Update Guesheftsbank.py

--- Guesheftsbank.py
+++ Guesheftsbank.py
@@ -16,7 +16,8 @@ while wotw != "exit": #wotw - это что?
 	for daynumber in range(cycle): #цикл - 252 рабочих дня
 		randnumloans = rnd.random
 		randnumdeposits = rnd.random #сначала задаём количество депозитов и кредитов на день
-		for app in range(appsperday):
+		for customer in range(randnumloans):
+		for customer in range(randnumdeposits):
 """Переменные:
 Заявка - Id, score - объединяются в датафрейм - генерируются во фрейме1, состоящем из 100 заявок, каждый день этот фрейм обнуляется, данные переписываются во фрейм2
 Score сравнивается с бенчмарком (30) - в MVP отказаться!!! 
