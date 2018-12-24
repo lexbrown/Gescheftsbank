f6d8dc79b8c749c8e6f083033737dfbb026ff3b5
LexBrown <44871845+lexbrown@users.noreply.github.com>
2018-12-24 23:16:50 +0300

    Update Guesheftsbank.py

--- Guesheftsbank.py
+++ Guesheftsbank.py
@@ -11,33 +11,39 @@ liabilities = 0
 placcount = 0
 assets = capital + liabilities
 days = 252
-id = 0
+id = 0 #Пусть клиентский айди начинается с 1
 accounts = pd.DataFrame(columns = ['AccType', 'ClientId', 'BeginDate', 'EndDate', 'BeginQ', 'EndQ', 'Status'])
+#день
 for i in range(days):
     randnumloans = random.choice(range(40))
     randnumdeposits = random.choice(range(40))
     for n in range(randnumloans):
         accType = 'L'
-        clientId = id
-        beginDate = i
-        endDate = i + 5
-        beginQ = 100000
+        clientId = id + 1
+        beginDate = i + 1 #это чтобы дня О не было
+        endDate = i + 6
+        beginQ = 1000
         endQ = beginQ * 1.06
         status = 'Active'
         newcostomer = [accType, clientId, beginDate, endDate, beginQ, endQ, status]
         accounts.loc[id] = newcostomer
         id += 1
+        liabilities += 1000
     for n in range(randnumdeposits):
         accType = 'D'
-        clientId = id
-        beginDate = i
-        endDate = i + 5
-        beginQ = 100000
+        clientId = id + 1
+        beginDate = i + 1
+        endDate = i + 6
+        beginQ = 1000
         endQ = beginQ * 1.03
         status = 'Active'
         newcostomer = [accType, clientId, beginDate, endDate, beginQ, endQ, status]
         accounts.loc[id] = newcostomer
         id += 1
+#вечер
+    for n in range(len(accounts)):
+        if accounts.loc[n, 'EndDate'] == i+1:
+            accounts.loc[n, 'Status'] = 'Closed'
 """Переменные:
 Заявка - Id, score - объединяются в датафрейм - генерируются во фрейме1, состоящем из 100 заявок, каждый день этот фрейм обнуляется, данные переписываются во фрейм2
 Score сравнивается с бенчмарком (30) - в MVP отказаться!!! 
