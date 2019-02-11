b26146cbd3ef1a62679361f52d2407a61f337c16
LexBrown <44871845+lexbrown@users.noreply.github.com>
2019-02-11 12:02:35 +0300

    Add files via upload

--- hints/pivot-olap.py
+++ hints/pivot-olap.py
@@ -1,5 +1,10 @@
-#Creating a new panel
+# New panel
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+rawdata = pd.read_excel('raw_set.xlsx')
+new_pivot  = pd.pivot_table(rawdata, values='Total sum', 
+                            index = 'Company', columns = 'Oil', aggfunc = np.sum)
+new_pivot.to_excel('new_pivot.xlsx')
