9e656c8fed7f4a75b88649f02e26b8dcfbb4bbe5
LexBrown <44871845+lexbrown@users.noreply.github.com>
2019-02-19 00:34:22 +0300

    Add files via upload

--- hints/Beta distribution.py
+++ hints/Beta distribution.py
@@ -1,10 +1,17 @@
 # Beta distribution
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
-asd = np.random.beta(2, 4, size = 1000)
+asd1 = np.random.beta(2, 4, size = 1000)
+asd2= [round(np.random.beta(2, 4), 1) + 0.1 for i in range(1000)]
 
-fug = plt.figure(figsize = (5,5))
-aux = fug.add_subplot(1,1,1)
-aux.hist(asd, bins = 20)
+fug = plt.figure(figsize = (10,5))
+aux1 = fug.add_subplot(1,2,1)
+aux1.hist(asd1, bins = 20)
+
+aux2 = fug.add_subplot(1,2,2)
+aux2.hist(asd2, bins = 10)
+
+pd.DataFrame(asd2).to_excel('test beta distribution.xlsx')
