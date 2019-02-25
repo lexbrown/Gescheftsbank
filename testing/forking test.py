fd437fc11ec058a5d7cad08c909ce156a8d3a311
LexBrown <44871845+lexbrown@users.noreply.github.com>
2019-02-25 14:11:14 +0300

    Update forking test.py

--- testing/forking test.py
+++ testing/forking test.py
@@ -8,3 +8,5 @@ import pymysql
 engine = create_engine('mysql+mysqldb://...', pool_recycle = 3600)
 #df = pd.DataFrame({'name': ['User1', 'User2', 'User3']})
 #df.to_sql('users', con = engine, if_exists = 'replace')
+
+#в итоге решение откопал здесь https://4admin.space/all/modulenotfounderror-no-module-named-mysqldb/
