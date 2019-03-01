4eaea95243905237de890976152bb17ace0af6dd
LexBrown <44871845+lexbrown@users.noreply.github.com>
2019-03-01 23:01:45 +0300

    Update Amazon - 2017.py

--- hints/Amazon - 2017.py
+++ hints/Amazon - 2017.py
@@ -1,8 +1,6 @@
 import numpy as np #optional
 import pandas as pd
 import matplotlib.pyplot as plt
-import scipy as sp
-
 
 amazon = pd.read_csv("from/amzn.csv", index_col=0, parse_dates=True)["Adj Close"]
 fig = plt.figure(figsize=(16, 8))
