64feee66aafc5935a7ffbbe5b045c6ce39c7175e
LexBrown <44871845+lexbrown@users.noreply.github.com>
2019-03-11 20:04:53 +0300

    Update External environment.py

--- hints/External environment.py
+++ hints/External environment.py
@@ -4,14 +4,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-x = np.arange(0, 101)
+x = np.arange(0, 201)
 df = pd.DataFrame(columns = ['Y', 'X'])
 for i in x:
-    df.loc[i] = [round((10 + 15 * np.sin(i / 5) + i) * (1 + np.random.randn()/5)), i]
-
-#y = (10 + 15 * np.sin(x / 5) + x) #* np.random.rand(100) #+ random.choice(range(111))
-#z = [(10 + 15 * np.sin(x / 5) + x) for i in range(1) ]
+    df.loc[i] = [round((30 + 15 * np.sin(i / 5) + i) * (1 + np.random.randn()/10)), i]
 
 figura = plt.figure(figsize=(15, 7))
 sub1 = figura.add_subplot(1,1,1)
-sub1.plot(df['Y'])
+sub1.plot(df['Y'], marker = 'o', color = 'red')
