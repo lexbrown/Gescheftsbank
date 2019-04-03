9c23a8dbc29942f6f596883e1c3f74738e5baa12
LexBrown <44871845+lexbrown@users.noreply.github.com>
2019-04-03 07:34:24 +0300

    Create Scatter pairs.py

---
+++ hints/Scatter pairs.py
@@ -0,0 +1,10 @@
+# scatter plots
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+scatterset = pd.read_excel('model final.xlsx')
+
+
+pd.plotting.scatter_matrix(scatterset, alpha = 1, s = 60,figsize = (16,16), diagonal = 'kde')
