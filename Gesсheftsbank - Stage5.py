83154e2ad4b2b37db26b45028bae2850288e2531
LexBrown <44871845+lexbrown@users.noreply.github.com>
2019-07-01 00:30:26 +0300

    Update Gesсheftsbank - Stage5.py

--- Gesсheftsbank - Stage5.py
+++ Gesсheftsbank - Stage5.py
@@ -6,7 +6,7 @@
 #   Дополнить проверки на балансовые показатели
 #   Написать парсер тянущий данные отсюда https://www.cbr.ru/hd_base/ruonia/
 
-import numpy as npр
+import numpy as np
 import pandas as pd
 import random
 import matplotlib.pyplot as plt
