42a0dfbd736542090889a4b01578e3e0ffca76c5
LexBrown <44871845+lexbrown@users.noreply.github.com>
2019-07-02 12:06:48 +0300

    Update Gesсheftsbank - Stage5.py

--- Gesсheftsbank - Stage5.py
+++ Gesсheftsbank - Stage5.py
@@ -9,7 +9,7 @@
 import numpy as np
 import pandas as pd
 import random
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pltо
 import mysql.connector as cnt
 from tqdm import tqdm
 #import statsmodels.api as sm
